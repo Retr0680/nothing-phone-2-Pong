723dec8b265b9503b88baf0d05e3b3841e229653
Retr0 <dedsecretr0680@gmail.com>
2024-03-01 21:55:41 +0530

    idk

---
+++ unlock_bootloader.bat
@@ -0,0 +1,21 @@
+@echo off
+
+title Nothing Phone 2 Fastboot ROM Flasher (t.me/NothingPhone2)
+
+echo ########################################################
+
+echo #        Nothing Phone 2 Fastboot ROM 		    #
+
+echo #               Developed And Tested By                #
+
+echo #     		Retr0_680			    #
+
+echo ########################################################
+
+fastboot devices
+
+fastboot flashing unlock
+
+fastboot flashing unlock_critical
+
+pause
