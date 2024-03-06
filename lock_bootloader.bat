8761638552ee9a42da94f7bc15255abc35dc46f8
Retr0 <hrithvikbhardwaj680@gmail.com>
2024-03-06 10:28:47 +0530

    Add files via upload

---
+++ lock_bootloader.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ########################
+echo #	Bootloader Lock 💀  #
+echo ########################
+
+fastboot devices
+
+fastboot flashing lock
+
+start https://github.com/Retr0680
+
+pause
